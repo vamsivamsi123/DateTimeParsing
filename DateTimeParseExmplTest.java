71c41489521a0451af6209417f69a3bfa4fc57a4
vamsivamsi123 <68283901+vamsivamsi123@users.noreply.github.com>
2020-07-15 10:29:39 +0530

    Add files via upload

---
+++ DateTimeParseExmplTest.java
@@ -0,0 +1,14 @@
+package dtparse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DateTimeParseExmplTest {
+
+	@Test
+	void test() {
+		System.out.println("pass");
+	}
+
+}
