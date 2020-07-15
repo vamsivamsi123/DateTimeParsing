f299835bd58ec2d2c067303c7ee922d2cba4a67a
vamsivamsi123 <68283901+vamsivamsi123@users.noreply.github.com>
2020-07-15 10:41:05 +0530

    Add files via upload

--- DateTimeParseExmpl.java
+++ DateTimeParseExmpl.java
@@ -1,9 +1,47 @@
 package dtparse;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class DateTimeParseExmpl {
 
-	public DateTimeParseExmpl() {
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) {
+		
+		args[0] = "2020-07-14 03:54:26 AM";
+		LocalDateTime localDT = null;
+		DateTimeFormatter myFormatObj;
+		try {
+
+			myFormatObj = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ssa");
+			localDT = LocalDateTime.parse(args[0], myFormatObj);
+
+		} catch (DateTimeParseException ex) {
+			System.out.println("wrong date pattern shoud be of dd-MM-yyyy HH:mm:ss:a ");
+		}
+		//DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder();
+		/*myFormatObj = builder.appendText(ChronoField.DAY_OF_MONTH, TextStyle.NARROW)
+				.appendText(ChronoField.MONTH_OF_YEAR, TextStyle.FULL).appendPattern("uuu").appendPattern("kk")
+				.appendText(ChronoField.MINUTE_OF_HOUR, TextStyle.NARROW_STANDALONE).toFormatter();
+		localDT.format(myFormatObj);*/
+		try {
+		System.out.println("word equivalent of date " + localDT.toLocalDate());
+		System.out.println("After formatting: time " + localDT.toLocalTime());
+		System.out.println("After formatting: week-" + localDT.getDayOfWeek());
+		} catch (Exception ex) {
+			
+		}
+		
+	}
+	
+	//test the Input String 
+	public void parseDateTimeInput(String args) throws DateTimeParseException {
+
+			DateTimeFormatter myFormatObj;
+			LocalDateTime localDT = null;
+			myFormatObj = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ssa");
+			localDT = LocalDateTime.parse(args, myFormatObj);
+					
 	}
 
 }
--- DateTimeParseExmplTest.java
+++ DateTimeParseExmplTest.java
@@ -1,14 +1,21 @@
 package dtparse;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.DateTimeException;
 
 import org.junit.jupiter.api.Test;
 
+
 class DateTimeParseExmplTest {
 
-	@Test
+	@Test ()
 	void test() {
-		System.out.println("pass");
+	
+		 DateTimeParseExmpl dtParse = new DateTimeParseExmpl();
+				
+		assertThrows(DateTimeException.class, () -> dtParse.parseDateTimeInput("2020-07-14 03:54:26 AM"), "invalid dateinput"); 
+		
 	}
 
 }
